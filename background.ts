769a3997a4e584c8a0e907427ee822a3ee09da6a
syab-syab <syabsyabsyabsyab@gmail.com>
2025-03-04 16:52:17 +0900

    backgroundでPromiseオブジェクトに苦戦中

--- background.ts
+++ background.ts
@@ -1,29 +1,46 @@
 // import { useStorage } from "@plasmohq/storage/hook"
-// import type { Word } from "~models/Word"
-// import { storageWordKey } from "~variables/storageWordKey"
+import type { Word } from "~models/Word"
+import { storageWordKey } from "~variables/storageWordKey"
+import { Storage } from "@plasmohq/storage"
+ 
+const storage = new Storage({
+  area: "sync" // "local"はローカル保存、"sync"はクラウド同期
+});
+
+
+// 二つともPromiseオブジェクトなのでどうにか中身を取り出して
+// push や JSON.parse などの関数を使いたい
+// ローカルデータ読み込み
+async function loadData() {
+  const value = await storage.get(storageWordKey);
+  // 配列をコピーしてから
+  // const tmpArr = JSON.parse(value).slice()
+  // console.log("保存されたデータ:", value);
+  // return tmpArr
+  return JSON.parse(value)
+}
+
+// ローカルに保存
+async function saveData(val: string) {
+  // 型を整形する
+  const tmpWord: Word = {
+    id: Date.now(),
+    word: val,
+    fav: false
+  }
+  const tmpArr = await storage.get(storageWordKey);
+  // 値を格納
+  JSON.parse(tmpArr).push(tmpWord)
+  await storage.set(storageWordKey, JSON.stringify(tmpArr));
+  console.log("データが保存されました");
+}
+
+
+
 // バックグラウンドでの処理
 // いずれもコンテキストメニューで
 // -- ドラッグしたワードを登録
 // -- 入力フォームにカーソルを合わせた際にお気に入りにしているワードをペーストできるようにする
-
-// useStorageの第二引数は初期値で、すでにstorageに値がある場合は無視されるっぽい
-// const [wordArr, setWordArr] = useStorage<string>(storageWordKey)
-
-// ワード追加(background向けに編集)
-// const addWordArr = (val: string) => {
-//   const tmpArr = JSON.parse(wordArr).slice()
-//   const tmpWord: Word = {
-//     id: Date.now(),
-//     word: val,
-//     fav: false
-//   }
-//   tmpArr.push(tmpWord)
-//   setWordArr(JSON.stringify(tmpArr))
-// }
-
-// エラー発生
-// コンテキストメニューが表示されない
-// useStorageを使うと不具合？
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
@@ -45,8 +62,10 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((info) => {
   if (info.menuItemId === "copy") {
     console.log(info.selectionText)
+    saveData(info.selectionText)
     // addWordArr(info.selectionText)
   } else if (info.menuItemId === "paste") {
-    console.log("ペーストテスト")
+    const value = loadData()
+    console.log(value)
   }
 })
