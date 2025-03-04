689561482e70fc13b68c0b55d93a5506b08d8c4a
syab-syab <syabsyabsyabsyab@gmail.com>
2025-03-04 15:35:05 +0900

    backgroundのコンテキストメニューの仮置きは出来た

--- background.ts
+++ background.ts
@@ -1,29 +1,25 @@
-import { useStorage } from "@plasmohq/storage/hook"
-import type { Word } from "~models/Word"
-import { storageWordKey } from "~variables/storageWordKey"
+// import { useStorage } from "@plasmohq/storage/hook"
+// import type { Word } from "~models/Word"
+// import { storageWordKey } from "~variables/storageWordKey"
 // バックグラウンドでの処理
 // いずれもコンテキストメニューで
 // -- ドラッグしたワードを登録
 // -- 入力フォームにカーソルを合わせた際にお気に入りにしているワードをペーストできるようにする
 
 // useStorageの第二引数は初期値で、すでにstorageに値がある場合は無視されるっぽい
-const [wordArr, setWordArr] = useStorage<string>(storageWordKey)
-// const [tmpData, setTmpData] = useState<string>("")
+// const [wordArr, setWordArr] = useStorage<string>(storageWordKey)
 
 // ワード追加(background向けに編集)
-const addWordArr = (val: string) => {
-  // 配列をコピーしてから
-  const tmpArr = JSON.parse(wordArr).slice()
-  // 型を整形する
-  const tmpWord: Word = {
-    id: Date.now(),
-    word: val,
-    fav: false
-  }
-  // 値を格納
-  tmpArr.push(tmpWord)
-  setWordArr(JSON.stringify(tmpArr))
-}
+// const addWordArr = (val: string) => {
+//   const tmpArr = JSON.parse(wordArr).slice()
+//   const tmpWord: Word = {
+//     id: Date.now(),
+//     word: val,
+//     fav: false
+//   }
+//   tmpArr.push(tmpWord)
+//   setWordArr(JSON.stringify(tmpArr))
+// }
 
 // エラー発生
 // コンテキストメニューが表示されない
@@ -36,12 +32,21 @@ chrome.runtime.onInstalled.addListener(() => {
     // selectionでドラッグしたときだけコンテキストメニュー表示
     // contexts: ["all"]
     contexts: ["selection"]
-  })  
+  })
+  // 子要素でお気に入りのワードを列挙する
+  chrome.contextMenus.create({
+    id: "paste",
+    title: "ワード貼り付け",
+    // editableでinput要素にカーソルが合わさった時に発動
+    contexts: ["editable"]
+  })
 })
 
 chrome.contextMenus.onClicked.addListener((info) => {
   if (info.menuItemId === "copy") {
     console.log(info.selectionText)
-    addWordArr(info.selectionText)
+    // addWordArr(info.selectionText)
+  } else if (info.menuItemId === "paste") {
+    console.log("ペーストテスト")
   }
 })
